e63ac95348ecbf8ec272851cea35407b449662d8
legoraft <legoraft@noreply.codeberg.org>
2024-06-05 11:47:45 +0200

    Added header parsing

--- src/header.rs
+++ src/header.rs
@@ -1,3 +1,6 @@
-pub fn parse(heading: &str) {
+pub fn parse(heading: &str) -> String {
+    let (count, text) = heading.split_once(" ").unwrap();
 
+    let header_tag = format!("<h{}>{}</h{}>", count.len(), text, count.len());
+    header_tag
 }
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ fn main() {
     let file = fs::read_to_string(&args[1])
         .expect("Couldn't open file!");
 
-    let paragraphs = paragraph::parse(file);
+    let mut paragraphs = paragraph::parse(file);
 
     for (id, paragraph) in paragraphs.iter().enumerate() {
         if paragraph.chars().nth(0).expect("Can't parse paragraph!") == '#' {
-            header::parse(&paragraph);
+            paragraphs[id] = header::parse(&paragraph);
         }
     }
 }
