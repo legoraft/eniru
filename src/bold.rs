3950036a7a778f24b9d754d0559b437265240117
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-05 14:38:17 +0200

    Added testing for bold text

--- src/bold.rs
+++ src/bold.rs
@@ -1,7 +1,21 @@
 use regex::Regex;
 
-fn parse(paragraph: String) {
+fn parse(paragraph: String) -> String {
     let bold_re = Regex::new(r"\*\*(?<s>.*)\*\*").unwrap();
-    bold_re.replace_all(&paragraph, "<b>$s</b>");
+    let paragraph = bold_re.replace_all(&paragraph, "<b>$s</b>").to_string();
+
+    paragraph
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_bold() {
+        let paragraph: String = "Hello, this is some text. I also have some **bold** text.".to_string();
+        let html: String = "Hello, this is some text. I also have some <b>bold</b> text.".to_string();
+
+        assert_eq!(html, parse(paragraph));
+    }
+}
