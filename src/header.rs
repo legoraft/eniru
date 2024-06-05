2ad086b18626e4b0eef57d7aa3a09ceee28a6bcc
legoraft <legoraft@noreply.codeberg.org>
2024-06-05 12:25:16 +0200

    Added some testing for headings

--- src/header.rs
+++ src/header.rs
@@ -4,3 +4,17 @@ pub fn parse(heading: &str) -> String {
     let header_tag = format!("<h{}>{}</h{}>", count.len(), text, count.len());
     header_tag
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_headings() {
+        let headings: &str = "\
+# This is a single heading";
+        let paragraphs: String = "<h1>This is a single heading</h1>".to_string();
+
+        assert_eq!(paragraphs, parse(headings));
+    }
+}
