2beea39f73914123e8e7a4655ff214aebcba426a
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-06 10:33:27 +0200

    Using types in compiling now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-use parser::{header, paragraph, styling};
+use parser::{header, paragraph, styling, ParagraphType};
 
 mod parser;
 
@@ -10,22 +10,19 @@ fn main() {
     let file = fs::read_to_string(&args[1])
         .expect("Couldn't open file!");
 
-    let paragraphs: Vec<paragraph::Paragraph> = paragraph::parse(file);
+    let paragraphs: Vec<parser::Paragraph> = parser::parse(file);
     let mut output_html: Vec<String> = Vec::new();
 
-/*    for paragraph in paragraphs {
-        if paragraph.chars().nth(0).expect("Can't parse paragraph!") == '#' {
-            let output = header::parse(&paragraph);
-            output_html.push(output);
-        } else {
-            let output = styling::parse(paragraph);
-            output_html.push("<p>".to_string());
-            output_html.push(output.clone());
-            output_html.push("</p>".to_string());
-        }
+    for paragraph in paragraphs {
+        let output = match paragraph.paragraph_type {
+            ParagraphType::Heading => parser::header::parse(&paragraph.text),
+            ParagraphType::Text => parser::styling::parse(paragraph.text),
+            _ => parser::styling::parse(paragraph.text),
+        };
+        
+        output_html.push(output);
     }
 
-*/
     for line in output_html {
         println!("{}", line);
     }
