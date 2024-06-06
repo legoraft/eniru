74a9d5bbc4b5e26f9fbd48487880cdff8b3b6ff4
legoraft <50689727+legoraft@users.noreply.github.com>
2024-06-06 11:47:12 +0200

    Changed input type to string, not &str

--- src/parser/heading.rs
+++ src/parser/heading.rs
@@ -1,6 +1,6 @@
 use super::styling;
 
-pub fn parse(heading: &str) -> String {
+pub fn parse(heading: String) -> String {
     let (count, text) = heading.split_once(" ").unwrap();
     let text = styling::parse(text.to_string());
 
@@ -14,8 +14,8 @@ mod tests {
 
     #[test]
     fn parse_large_heading() {
-        let headings: &str = "\
-# This is a single heading";
+        let headings: String = "\
+# This is a single heading".to_string();
         let paragraphs: String = "<h1>This is a single heading</h1>".to_string();
 
         assert_eq!(paragraphs, parse(headings));
@@ -23,8 +23,8 @@ mod tests {
 
     #[test]
     fn parse_small_heading() {
-        let headings: &str = "\
-### This is a level 3 heading";
+        let headings: String = "\
+### This is a level 3 heading".to_string();
         let paragraphs: String = "<h3>This is a level 3 heading</h3>".to_string();
 
         assert_eq!(paragraphs, parse(headings));
