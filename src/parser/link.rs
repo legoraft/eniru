35eb714ae3b17e8c865efd3fc25e0d5f1f5564f5
legoraft <legoraft@legoraft.com>
2024-06-13 13:19:15 +0200

    Added link parsing

---
+++ src/parser/link.rs
@@ -0,0 +1,18 @@
+fn parse_link(text: String) -> String {
+    let link_re = Regex::new(r"[(?<t>.*)]((?<l>.*))").unwrap();
+    let text = link_re.replace_all(&text, "<a href=\"$l\">$t</a>").to_string();
+
+    text
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn link_parsing() {
+        let paragraph: String = "\
+This is a small paragraph with a simple [link](../homepage.md).".to_string();
+        let paragraphs: String = "<p>This is a small paragraph with a simple <a href=\"../homepage.md\">link</a>".to_string();
+
+        assert_eq!(paragraphs, parse(paragraph));
+    }
+}
