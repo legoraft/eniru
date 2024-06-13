ba2125dd4f3df2aa9543ea698d633ac26ec0bfe9
legoraft <legoraft@legoraft.com>
2024-06-13 12:51:18 +0200

    Added inline code support

--- src/parser/styling.rs
+++ src/parser/styling.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 pub fn parse(paragraph: String) -> String {
     let paragraph: String = parse_bold(paragraph);
     let paragraph: String = parse_italic(paragraph);
+    let paragraph: String = parse_code(paragraph);
 
     paragraph
 }
@@ -17,6 +18,13 @@ fn parse_bold(text: String) -> String {
 fn parse_italic(text: String) -> String {
     let italic_re = Regex::new(r"_(?<i>.*)_").unwrap();
     let text = italic_re.replace_all(&text, "<em>$i</em>").to_string();
+
+    text
+}
+
+fn parse_code(text: String) -> String {
+    let code_re = Regex::new(r"`(?<c>.*)`").unwrap();
+    let text = code_re.replace_all(&text, "<code>$c</code>").to_string();
 
     text
 }
@@ -42,9 +50,17 @@ mod tests {
     }
 
     #[test]
+    fn code_parsing() {
+        let paragraph: String = "Hello, this is some text. I also have this `function()` to explain.".to_string();
+        let html: String = "Hello, this is some text. I also have this <code>function()</code> to explain.".to_string();
+
+        assert_eq!(html, parse_code(paragraph));
+    }
+
+    #[test]
     fn style_parsing() {
-        let paragraph: String = "This is a simple sentence to check if both **bold** and _italic_ text work within Eniru.".to_string();
-        let html: String = "This is a simple sentence to check if both <strong>bold</strong> and <em>italic</em> text work within Eniru.".to_string();
+        let paragraph: String = "This is a simple sentence to check if both **bold** and _italic_ text work within Eniru. I also want to test this example `function()`.".to_string();
+        let html: String = "This is a simple sentence to check if both <strong>bold</strong> and <em>italic</em> text work within Eniru. I also want to test this example <code>function()</code>.".to_string();
 
         assert_eq!(html, parse(paragraph));
     }
