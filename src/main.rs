ed0b7fe6d1ca3cc275366522969845c9320aa274
legoraft <legoraft@legoraft.com>
2024-06-15 13:11:43 +0200

    fix: added better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{env, fs};
+use std::{env, fs, io::ErrorKind, path::Path};
 
 use eniru::file_generator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file = env::set_current_dir(&args[1])
-        .expect("Couldn't move into specified directory!");
+    env::set_current_dir(&args[1]).expect("Couldn't move into specified directory!");
     
     let posts = fs::read_dir("./posts").expect("Couldn't find post directory!");
-    fs::create_dir("./site/").expect("Couldn't initialize site!");
+    
+    if !Path::new("./site").exists() {
+        fs::create_dir("./site").expect("Couldn't create site directory!");
+    }
 
     file_generator::generate(posts);
 }
