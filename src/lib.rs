7a13f723694939f1a6a027b92a0eaa4d6688a778
legoraft <legoraft@legoraft.com>
2024-06-15 11:21:03 +0200

    refactor: Moving markdown parsing to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use parser::{code, heading, list, paragraph, ParagraphType};
+
+mod parser;
+
+fn parse(file: String) -> String {
+    let paragraphs: Vec<parser::Paragraph> = parser::parse(file);
+    let mut output_html: Vec<String> = Vec::new();
+    let mut output_string: String = String::new();
+
+    for paragraph in paragraphs {
+        let output = match paragraph.paragraph_type {
+            ParagraphType::Heading => heading::parse(paragraph.text),
+            ParagraphType::List => list::parse(paragraph.text),
+            ParagraphType::Text => paragraph::parse(paragraph.text),
+            ParagraphType::Code => code::parse(paragraph.text),
+        };
+        
+        output_html.push(output);
+    }
+
+    for line in output_html {
+        output_string.push_str(&[&line, "\n"].concat());
+    }
+    
+    output_string
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,10 @@
 use std::{env, fs};
 
-use parser::{code, heading, list, paragraph, ParagraphType};
-
-mod parser;
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let file = fs::read_to_string(&args[1])
         .expect("Couldn't open file!");
-
-    let paragraphs: Vec<parser::Paragraph> = parser::parse(file);
-    let mut output_html: Vec<String> = Vec::new();
-
-    for paragraph in paragraphs {
-        let output = match paragraph.paragraph_type {
-            ParagraphType::Heading => heading::parse(paragraph.text),
-            ParagraphType::List => list::parse(paragraph.text),
-            ParagraphType::Text => paragraph::parse(paragraph.text),
-            ParagraphType::Code => code::parse(paragraph.text),
-        };
-        
-        output_html.push(output);
-    }
-
-    for line in output_html {
-        println!("{}", line);
-    }
+    
+    let html = 
 }
