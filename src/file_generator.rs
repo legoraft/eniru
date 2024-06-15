d34d5cc3e680ce6e7979a95081b24e0e053a4146
legoraft <legoraft@legoraft.com>
2024-06-15 12:35:24 +0200

    fix: site generation is fully working

--- src/file_generator.rs
+++ src/file_generator.rs
@@ -1,30 +1,26 @@
-use std::{env::{self, current_dir}, fs::{self, ReadDir}, path::PathBuf};
+use std::{env::{self, current_dir, set_current_dir}, fs::{self, ReadDir}, path::PathBuf};
 
 use crate::parse_markdown;
 
 pub fn generate(posts: ReadDir) {
     fs::create_dir("./site/posts/").expect("Couldn't create posts directory!");
-    println!("{:?}", current_dir().unwrap());
+    let working_dir = current_dir().unwrap();
+    let site_posts_dir = [working_dir.to_str().unwrap(), "/site/posts"].concat();
 
     for post in posts {
-        env::set_current_dir("./site/posts").expect("Couldn't move into posts dir!");
-
-        if current_dir().unwrap() == PathBuf::from("./site/posts") {
-            println!("{:?}", current_dir().unwrap());
+        if current_dir().unwrap() == PathBuf::from(&site_posts_dir) {
+            set_current_dir(&working_dir).expect("Couldn't move to working directory");
         }
 
-        /* let path = post.expect("Couldn't get file path!").path();
-        println!("{:?}", &path);
+        let path = post.expect("Couldn't get file path!").path();
         let file = fs::read_to_string(&path).expect("Couldn't read markdown file!");
         let html = parse_markdown(file);
 
         let filename = path.file_stem().unwrap();
         let output_file = [filename.to_str().unwrap(), ".html"].concat();
         
-        fs::create_dir("./site/posts/").expect("Couldn't create posts directory!");
-        env::set_current_dir("./site/posts/").expect("Couldn't move into dir!");
+        set_current_dir(&site_posts_dir).expect("Couldn't move into posts dir!");
         fs::write(output_file, html).expect("Couldn't write file!");
-        */
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use std::{env, ffi::OsString, fs, path::Path};
+use std::{env, fs};
 
-use eniru::{file_generator, parse_markdown};
+use eniru::file_generator;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file = env::set_current_dir(&args[1])
+        .expect("Couldn't move into specified directory!");
+    
     let posts = fs::read_dir("./posts").expect("Couldn't find post directory!");
     fs::create_dir("./site/").expect("Couldn't initialize site!");
 
